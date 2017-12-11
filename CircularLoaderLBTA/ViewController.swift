7ac0f2ed078cf6c25918223526a14ea955e4a0db
johnrm9 <johnrm9@gmail.com>
2017-12-11 08:29:47 -0800

    Added progress label in center of view (circle)

--- CircularLoaderLBTA/ViewController.swift
+++ CircularLoaderLBTA/ViewController.swift
@@ -10,32 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let shapeLayer: CAShapeLayer = {
+    let percentageLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Start"
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 32)
+        return label
+    }()
+
+    
+    lazy var shapeLayer: CAShapeLayer = {
         let layer = CAShapeLayer()
         layer.strokeColor = UIColor.red.cgColor
         layer.fillColor = UIColor.clear.cgColor
         layer.lineWidth = 10
         layer.lineCap = kCALineCapRound
         layer.strokeEnd = 0
+        layer.position = self.view.center
+        layer.transform = CATransform3DMakeRotation(-.pi / 2, 0, 0, 1)
         return layer
     }()
     
-    let trackLayer: CAShapeLayer = {
+    lazy var trackLayer: CAShapeLayer = {
         let layer = CAShapeLayer()
         layer.strokeColor = UIColor.lightGray.cgColor
         layer.fillColor = UIColor.clear.cgColor
         layer.lineWidth = 10
+        layer.position = self.view.center
         return layer
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .black
+        view.backgroundColor = .white
         
-        let center = view.center
+        view.addSubview(percentageLabel)
+        percentageLabel.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        percentageLabel.center = view.center
         
-        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: -.pi/2, endAngle: 2 * .pi, clockwise: true)
+        //let center = view.center
+        
+        let circularPath = UIBezierPath(arcCenter: .zero, radius: 100, startAngle: 0, endAngle: 2 * .pi, clockwise: true)
         
         shapeLayer.path = circularPath.cgPath
         trackLayer.path = circularPath.cgPath
